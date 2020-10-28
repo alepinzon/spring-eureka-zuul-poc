4c876b42932ec1767fc4c4699a98cdca6d0172c7
Alejandro Pinzon <jpinzon@healthcare.com>
2020-10-28 16:57:22 -0600

    first commit
    first commit

---
+++ foo-api/src/main/java/com/example/FooController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return String.format("Hello from '%s':'%s'!", eurekaClient.getApplication(appName).getName(), serverPort);
+    }
+}
